70f771827cb9a725a592072a9ce0eb01d8c6af14
Gord Bolli <gord@bolli.ca>
2025-08-10 11:06:36 -0700

    accept prompt, connect to Gemini genai client to get response, report tokens used

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+
 def main():
     print("Hello from ai-agent!")
 
+    if len(sys.argv) < 2:
+        print("No arguments provided. Please include a prompt.")
+        sys.exit(1)
+
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001', contents=str(sys.argv[1])
+    )
+    print(response.text)
+
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    print("Response tokens:", response.usage_metadata.candidates_token_count)
+    
 
 if __name__ == "__main__":
     main()
