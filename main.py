3a1b5a6ea6a49cb224bb258d1c97276700a8d3b1
Gord Bolli <gord@bolli.ca>
2025-08-11 09:47:51 -0700

    save user prompt to messages list, call model using messages

--- main.py
+++ main.py
@@ -2,22 +2,35 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+
 
 def main():
     print("Hello from ai-agent!")
 
+    # check if a prompt is provided
     if len(sys.argv) < 2:
         print("No arguments provided. Please include a prompt.")
         sys.exit(1)
 
+    # get the user prompt from command line arguments
+    user_prompt = sys.argv[1]
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    # set up genai client
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
-
     client = genai.Client(api_key=api_key)
 
+    # call the Gemini model
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents=str(sys.argv[1])
+        model='gemini-2.0-flash-001', 
+        contents=messages
     )
+
+    # print the response
     print(response.text)
 
     print("Prompt tokens:", response.usage_metadata.prompt_token_count)
